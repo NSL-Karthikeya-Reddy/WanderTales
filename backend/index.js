9b9247891b13a5d0b8ae84ec5f64f53cf231322a
NSL-Karthikeya-Reddy <karthikeyareddy2nd@gmail.com>
2025-02-16 02:06:26 +0530

    Update CORS allowed headers to include Access-Control-Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cors({
   origin: ['https://wander-tales-frontend.vercel.app', 'http://localhost:5173/'],
   methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
+  allowedHeaders: 'Content-Type,Authorization, Access-Control-Allow-Origin',
 }));
 
 // Create Account
