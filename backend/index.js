ae3e157bd3390475c2181fe04a59a55630469806
NSL-Karthikeya-Reddy <karthikeyareddy2nd@gmail.com>
2025-02-16 02:49:23 +0530

    Update CORS allowed headers to include Access-Control-Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ mongoose.connect(config.connectionString, {
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
     credentials: true
   }));
   
